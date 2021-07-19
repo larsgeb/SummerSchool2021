036f212db9c14581d21e656e06085c29db5a50d7
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2021-07-19 12:07:23 +0200

    Monday 19-07 changes.

--- topics/cuda/practicals/demos/hello.cu
+++ topics/cuda/practicals/demos/hello.cu
@@ -7,7 +7,27 @@ void hello_kernel() {
 
 int main(void) {
     hello_kernel<<<1, 32>>>();
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n",
+               cudaGetErrorString(cudaerr));
+
+    double* ptr;
+
+    auto size = 10 * sizeof(double);
+    double *ptr_host = (double*)malloc(size);
+
+
+    cudaMalloc(&ptr, size);
+
+    cudaFree(ptr);
+
+    cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n",
+               cudaGetErrorString(cudaerr));
+
     return 0;
 }
 
